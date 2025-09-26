06e7be4ce28052c3b290586e7a3b2e1316aa02d2
Erick Ramon Hernandez Ortega <erickramon47@live.com.mx>
2025-09-25 20:48:40 -0600

    feat: add create product and handle error

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  private readonly logger: Logger = new Logger(ProductsService.name);
+
+  // Repository patron
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) { }
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product: Product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+      return product;
+    } catch (error: any) {
+      this.handleExeptions(error);
+    }
   }
 
   findAll() {
@@ -22,5 +40,14 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleExeptions(error: any) {
+    if (error?.code === '23505') {
+      throw new BadRequestException(`${error?.detail}`)
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException(`Error: ${error}`);
   }
 }
