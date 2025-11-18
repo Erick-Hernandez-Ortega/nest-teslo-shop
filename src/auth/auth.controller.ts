0d3b232001dbab7efbdfca7cbf724ebfaaa0bc59
Erick Ramon Hernandez Ortega <erickramon47@live.com.mx>
2025-11-17 20:02:08 -0600

    feat: add another way on validate rol, with custom decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,9 @@ import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
 import { RawHeaders } from './decorators/raw-headers.decorator';
 import { UserRoleGuard } from './guards/user-role.guard';
+import { RoleProtected } from './decorators/role-protected.decorator';
+import { ValidRoles } from './interfaces/valid-roles';
+import { UserRole2Guard } from './guards/user-role2.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -38,6 +41,15 @@ export class AuthController {
   @SetMetadata('roles', ['admin', 'super-admin'])
   @UseGuards(AuthGuard(), UserRoleGuard)
   testingPrivateRoute2() {
+    return {
+      ok: true
+    }
+  }
+
+  @Get('private3')
+  @RoleProtected(ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRole2Guard)
+  testingPrivateRoute3() {
     return {
       ok: true
     }
